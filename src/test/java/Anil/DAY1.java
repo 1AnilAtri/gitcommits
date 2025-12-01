1ded24b6ac961de7397bc70cb3400c8e8483b50e
1AnilAtri <32anilkumar11s2@gmail.com>
2025-12-01 13:37:07 +0530

    third commit

--- src/test/java/Anil/DAY1.java
+++ src/test/java/Anil/DAY1.java
@@ -1,8 +1,13 @@
 package Anil;
 
 public class DAY1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("First Commit");
         System.out.println("Second Commit");
+        int number = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
     }
 }
